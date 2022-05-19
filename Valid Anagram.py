c14125bb4a76b0dd58586f2dd796b95569034cf8
nanup <nanup.personal+github@gmail.com>
2022-05-19 16:51:37 +0530

    adds two more easy leets

---
+++ Valid Anagram.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        if len(s) != len(t):
+            return False
+        dictS = Solution().dictionarise(s)
+        dictT = Solution().dictionarise(t)
+        
+        for i in dictS.keys():
+            try:
+                if dictS[i] == dictT[i]:
+                    continue
+                return False
+            except:
+                return False
+            
+        return True
+        
+    def dictionarise(self, aString):
+        dict = {}
+        for ch in aString:
+            print(ch)
+            if ch in dict:
+                dict[ch] += 1
+                print(dict)
+            else:
+                dict[ch] = 1
+        return dict
+
+print(Solution().isAnagram('aacc', 'ccac'))

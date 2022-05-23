6d536fbdd785ade7af85ffb4f628244cd3908522
nanup <nanup.personal+github@gmail.com>
2022-05-23 19:40:33 +0530

    adds dynamic programming to maximum subarray

--- Week 2/Maximum Subarray.py
+++ Week 2/Maximum Subarray.py
@@ -16,3 +16,21 @@ class Solution(object):
             maxSum = max(maxSum, currentSum)
             
         return maxSum
+
+# Dynamic Programming
+
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        subArraySums = [0] * len(nums)
+        subArraySums[0], maxSum = nums[0], nums[0]
+        
+        for i in range(1, len(nums)):
+            subArraySums[i] = max(nums[i], subArraySums[i - 1] + nums[i])
+            maxSum = max(maxSum, subArraySums[i])
+            
+        return maxSum
